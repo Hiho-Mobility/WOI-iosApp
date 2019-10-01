c8a9a0b26f731a6dd584c55e9cc1c6f4f3519076
Hussein <hznagri@uwaterloo.ca>
2019-10-01 11:28:04 -0400

    initial app setup

---
+++ WOI/WOI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WOI
+//
+//  Created by Hussein Nagri on 2019-10-01.
+//  Copyright © 2019 Hussein Nagri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
